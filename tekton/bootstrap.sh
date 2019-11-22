412912669594f6c014c203f7b3a39cf769100406
Kevin McDermott <kmcdermo@redhat.com>
2019-11-22 15:27:04 +0000

    Add new binding and templates.

--- tekton/bootstrap.sh
+++ tekton/bootstrap.sh
@@ -2,6 +2,7 @@
 oc apply -f https://github.com/tektoncd/pipeline/releases/download/v0.8.0/release.yaml
 oc apply -f https://github.com/tektoncd/triggers/releases/download/v0.1.0/release.yaml
 oc apply -f ~/quayio-secret.yaml
+oc new-project dev-environment
 oc new-project cicd-environment
 oc adm policy add-scc-to-user privileged -z demo-sa
 oc adm policy add-role-to-user edit -z demo-sa
