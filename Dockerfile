561e2fdcd97f6867d2cb91bab9d05a7b296b51ab
Kevin McDermott <kmcdermo@redhat.com>
2019-11-22 09:32:24 +0000

    Rearrange to ensure that the namespace exists before creating the service account.

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,6 @@
 FROM golang:latest AS build
 WORKDIR /go/src
-COPY ./src /go/src
+COPY . /go/src
 RUN GIT_COMMIT=$(git rev-list -1 HEAD) && go build -o booktaxi -ldflags "-X main.CommitSHA=$GIT_COMMIT" ./src/cmd/booktaxi
 
 FROM registry.access.redhat.com/ubi8/ubi-minimal
--- tekton/bootstrap.sh
+++ tekton/bootstrap.sh
@@ -2,11 +2,11 @@
 oc apply -f https://github.com/tektoncd/pipeline/releases/download/v0.8.0/release.yaml
 oc apply -f https://github.com/tektoncd/triggers/releases/download/v0.1.0/release.yaml
 oc apply -f ~/quayio-secret.yaml
-oc apply -f serviceaccount
+oc new-project cicd-environment
 oc adm policy add-scc-to-user privileged -z demo-sa
 oc adm policy add-role-to-user edit -z demo-sa
+oc apply -f serviceaccount
 oc apply -f templatesandbindings
-oc new-project cicd-environment
 oc apply -f interceptor
 oc apply -f eventlisteners
 oc apply -f ../build
