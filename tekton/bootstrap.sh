94d18bb418c6bba676b92c72a9fbe6ede0eddb3e
Kevin McDermott <kmcdermo@redhat.com>
2019-11-25 10:42:36 +0000

    Fixes.

--- tekton/bootstrap.sh
+++ tekton/bootstrap.sh
@@ -6,6 +6,7 @@ oc new-project dev-environment
 oc new-project cicd-environment
 oc adm policy add-scc-to-user privileged -z demo-sa
 oc adm policy add-role-to-user edit -z demo-sa
+k create rolebinding demo-sa-admin-dev --clusterrole=admin --serviceaccount=cicd-environment:demo-sa --namespace=dev-environment
 oc apply -f serviceaccount
 oc apply -f templatesandbindings
 oc apply -f interceptor
