0b195a365f0fe3cad40ec6e0c05fd9bf54d05af3
Kevin McDermott <kmcdermo@redhat.com>
2019-11-26 11:48:54 +0000

    Rename the tasks and pipelines.

--- pipelines/bootstrap.sh
+++ pipelines/bootstrap.sh
@@ -1,19 +1,21 @@
 #!/bin/sh
 oc apply -f https://github.com/tektoncd/pipeline/releases/download/v0.8.0/release.yaml
 oc apply -f https://github.com/tektoncd/triggers/releases/download/v0.1.0/release.yaml
-oc apply -f ~/quayio-secret.yaml
 oc new-project dev-environment
+oc new-project stage-environment
 oc new-project cicd-environment
+oc apply -f ~/quayio-secret.yaml
+oc apply -f ~/regcred.yaml
+oc apply -f serviceaccount
 oc adm policy add-scc-to-user privileged -z demo-sa
 oc adm policy add-role-to-user edit -z demo-sa
-k create rolebinding demo-sa-admin-dev --clusterrole=admin --serviceaccount=cicd-environment:demo-sa --namespace=dev-environment
-k create rolebinding demo-sa-admin-stage --clusterrole=admin --serviceaccount=cicd-environment:demo-sa --namespace=stage-environment
-oc apply -f serviceaccount
+kubectl create rolebinding demo-sa-admin-dev --clusterrole=admin --serviceaccount=cicd-environment:demo-sa --namespace=dev-environment
+kubectl create rolebinding demo-sa-admin-stage --clusterrole=admin --serviceaccount=cicd-environment:demo-sa --namespace=stage-environment
 oc apply -f templatesandbindings
 oc apply -f interceptor
-oc apply -f eventlisteners
 oc apply -f ci
 oc apply -f cd
+oc apply -f eventlisteners
 echo "the following commands will need to be executed when the containers are all running"
 echo "oc port-forward svc/el-cicd-event-listener 8080"
 echo "./demo.sh"
