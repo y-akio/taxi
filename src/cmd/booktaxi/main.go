b3c55d978ba1fea77ed78f200444dc9d0339b5e6
Kevin McDermott <kmcdermo@redhat.com>
2019-11-27 12:43:11 +0000

    Add a Demoversion header with the extracted SHA.

--- src/cmd/booktaxi/main.go
+++ src/cmd/booktaxi/main.go
@@ -18,9 +18,16 @@ func main() {
 	flag.Parse()
 
 	fs := http.FileServer(http.Dir(*wwwRoot))
-	http.Handle("/", fs)
+	http.Handle("/", addVersionHeader(fs))
 
 	addr := fmt.Sprintf(":%d", *port)
 	log.Printf("version %s listening on %s\n", CommitSHA[:6], addr)
 	log.Fatal(http.ListenAndServe(addr, nil))
+}
+
+func addVersionHeader(h http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Add("DemoVersion", CommitSHA)
+		h.ServeHTTP(w, r)
+	})
 }
